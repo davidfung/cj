1cd7a91dacadd498775dedbfb65fcff4086af536
davidfung <davidfung@amgcomputing.com>
2023-05-24 21:32:40 -0700

    Restructure db code to prepare for score keeping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-use database::CJDatabase;
+use database::{CJDatabase, Chinese};
 
 mod database;
 
@@ -12,17 +12,15 @@ fn ask(prompt: &str, chinchar: &String) -> bool {
     chinchar == line.trim()
 }
 
-fn run(db: &mut CJDatabase) {
+fn run(items: Vec<Chinese>) -> Vec<Chinese> {
     let mut score = 0;
     let mut count = 0;
     let max_count = 10;
 
-    let questions = db.get_questions();
-
     println!("\n\n\n======== T E S T  B E G I N ========");
     let now = Instant::now();
 
-    for chin in questions.iter() {
+    for chin in items.iter() {
         count = count + 1;
         if count > max_count {
             break;
@@ -42,14 +40,18 @@ fn run(db: &mut CJDatabase) {
 
     let elapsed_time = now.elapsed();
     println!("Time taken: {} seconds", elapsed_time.as_secs());
+
+    items
 }
 
 fn main() {
-    let mut db = database::CJDatabase { v: Vec::new() };
+    let mut db = CJDatabase { v: Vec::new() };
     db.load();
 
     loop {
-        run(&mut db);
+        let items = db.get_questions();
+        let results = run(items);
+        //db.update(results);
         db.save();
     }
 }
